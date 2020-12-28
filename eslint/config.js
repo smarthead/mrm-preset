99b63612088aeebc0ff0b1e202cca0f766c4b81f
Tikhonov Nikita <tn@smarthead.ru>
2020-12-28 15:58:11 +0300

    ESLint. Add fix label rules

--- eslint/config.js
+++ eslint/config.js
@@ -1,12 +1,14 @@
 const config = {
     ts: {
         rules: {
-            curly: [2, 'all'],
+            curly: 2,
         },
     },
     reactTs: {
         rules: {
             'react/prop-types': 0,
+            'jsx-a11y/label-has-for': 0,
+            'jsx-a11y/label-has-associated-control': 2,
         }
     },
 };
