2d603ac66eb1f8d35bce16055ff8402cd22d8c0a
Nikita Tikhonov <tn@smarthead.ru>
2022-09-20 15:35:17 +0300

    Updates

--- -eslint/config.js
+++ -eslint/config.js
@@ -2,6 +2,7 @@ const config = {
     ts: {
         rules: {
             curly: 2,
+            "@typescript-eslint/no-explicit-any": 0,
         },
     },
     reactTs: {
@@ -9,7 +10,7 @@ const config = {
             'react/prop-types': 0,
             'jsx-a11y/label-has-associated-control': 2,
             'jsx-a11y/label-has-for': 0,
-            'jsx-a11y/no-autofocus': 1
+            'jsx-a11y/no-autofocus': 1,
         },
     },
 };
